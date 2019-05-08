8a9894210e6e18e71fa976c705373773aa4af076
Conor Owens <conorj.owens@gmail.com>
2019-05-08 00:05:15 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,19 +1,18 @@
 class School
   
-  roster = {}
-  
   def initialize(name)
     @name = name
+    @roster = {}
   end
   
   attr_reader :name
   
   def add_student(name, grade)
-    if roster[grade]
-      roster[grade] << name
+    if @roster[grade]
+      @roster[grade] << name
     else
-      roster[grade]
-      roster[grade] << name
+      @roster[grade]
+      @roster[grade] << name
     end
   end
   
